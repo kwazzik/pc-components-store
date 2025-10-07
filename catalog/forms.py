fc811b52b68518de747615dba3de86ed803a4272
chtot <mishakwazzi@gmail.com>
2025-10-07 16:33:30 +0300

    Fix form field in ProductSearchForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -46,7 +46,7 @@ def validate_employee_number(employee_number):
 
 
 class ProductSearchForm(forms.Form):
-    model = forms.CharField(
+    name = forms.CharField(
         max_length=255,
         required=False,
         label="",
