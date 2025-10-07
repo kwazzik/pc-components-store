dfdf9d928f05d627ec59628430ac0ed78ebd673a
chtot <mishakwazzi@gmail.com>
2025-10-07 16:02:15 +0300

    Move SECRET_KEY to environment variable for better security

--- pc_components_store/settings.py
+++ pc_components_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from catalog import apps
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ebdpi!3_p&f*%!)p79p^!=1v)ef=3sdm)f@x20!wb$7ycy$@r'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
