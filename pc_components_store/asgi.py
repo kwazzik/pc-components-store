752bc4d0936f8c04af42807c7d2e56d6628017eb
chtot <mishakwazzi@gmail.com>
2025-10-08 21:39:11 +0300

    delete .prod from asgi.py

--- pc_components_store/asgi.py
+++ pc_components_store/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pc_components_store.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pc_components_store.settings')
 
 application = get_asgi_application()
