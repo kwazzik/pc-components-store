f6651031f8deb9870156d1f6961baf3b1ba852d0
chtot <mishakwazzi@gmail.com>
2025-10-08 13:40:45 +0300

    staticfiles managing

--- pc_components_store/settings/base.py
+++ pc_components_store/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib import staticfiles
 from dotenv import load_dotenv
 
 from catalog import apps
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
